e86b69f7225d33bedc5a2968e1700c1b259b896b
codewithnuh <nuh25792@gmail.com>
2024-11-24 08:03:12 +0500

    feat(auth, user): added Prisma and SQLite for database management, introduced auth and user modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ControllerModule } from './service/controller/controller.module';
+import { AuthModule } from './auth/auth.module';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+import { TodoController } from './todo/todo.controller';
+import { TodoService } from './todo/todo.service';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ControllerModule, AuthModule, UserModule, TodoModule],
+  controllers: [AppController, UserController, TodoController],
+  providers: [AppService, TodoService],
 })
 export class AppModule {}
